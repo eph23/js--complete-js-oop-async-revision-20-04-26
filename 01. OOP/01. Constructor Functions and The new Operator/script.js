ea8332227a952558586e2b332822c8e037250455
Eph23 <ephraims.818@gmail.com>
2026-04-20 22:21:50 +0600

    JS--OOP-ASYNC-REVISE -- OOP -- 01. Constructor Functions and The new Operator

---
+++ 01. OOP/01. Constructor Functions and The new Operator/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const eph = new Person('Ephraim', 1988);
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 2075);
+
+console.log(eph);
+console.log(matilda);
+console.log(jack);
+
+console.log(eph instanceof Person);
